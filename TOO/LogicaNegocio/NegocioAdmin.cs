b5877fbf3d489d5636ca7644bf1e0ba67358b96f
mario <mario10tatu@hotmail.es>
2020-12-30 17:07:22 +0100

    Actualizacion NegocioAdmin

--- TOO/LogicaNegocio/NegocioAdmin.cs
+++ TOO/LogicaNegocio/NegocioAdmin.cs
@@ -8,9 +8,103 @@ namespace LogicaNegocio
 {
     public class NegocioAdmin
     {
-        public void agua()
+
+        //-------------------------------------------------------------------------------
+        //Gestion de Clientes
+
+        public void darAltaCliente()
         {
             ModeloDominio.
+        }
+
+        public void darBajaCliente()
+        {
+            
+        }
+
+        public void obtenerPresupuestos()
+        {
+
+        }
+
+        public void obtenerInfoCliente()
+        {
+
+        }
+
+        //------------------------------------------------------------------------------
+        //Gestion de Vehiculos
+
+        public void darAltaVehiculo()
+        {
+
+        }
+
+        public void darBajaVehiculo()
+        {
+
+        }
+
+        public void obtenerInfoVehiculo()
+        {
+
+        }
+
+        public void obtenerTodosVehiculos()
+        {
+
+        }
+
+        public void obtenerVehiculosSegundaMano()
+        {
+
+        }
+
+        //------------------------------------------------------------------------------
+        //Gestion de Presupuestos
+
+        public void crearPresupuesto()
+        {
+
+        }
+
+        public void obtenerDatosClientePresupuesto()
+        {
+
+        }
+
+        public void obtenerDatosVehiculoPresupuesto()
+        {
+
+        }
+
+        public void obtenerConfirmacionPresupuesto()
+        {
+
+        }
+
+        public void obtenerEstadoPresupueto()
+        {
+
+        }
+
+        public void eliminarPresupuesto()
+        {
+
+        }
+
+        public void obtenerDatosPresupuesto()
+        {
+
+        }
+
+        public void obtenerTodosPresupuestosVehiculo() { 
+        
+        }
+
+        public void obtenerTodosPresupuestosPorEstado()
+        {
+
         }
     }
 }
