463e0e927e484590d9ef802d1665e47ab9ee661b
mario <mario10tatu@hotmail.es>
2021-01-16 12:56:59 +0100

    Actualizacion de la interfaz gráfica

--- TOO/Presentación/AltaCliente.Designer.cs
+++ TOO/Presentación/AltaCliente.Designer.cs
@@ -45,58 +45,65 @@
             // 
             // dnitb
             // 
-            this.dnitb.Location = new System.Drawing.Point(197, 62);
+            this.dnitb.Location = new System.Drawing.Point(131, 40);
+            this.dnitb.Margin = new System.Windows.Forms.Padding(2);
             this.dnitb.Name = "dnitb";
-            this.dnitb.Size = new System.Drawing.Size(125, 26);
+            this.dnitb.Size = new System.Drawing.Size(85, 20);
             this.dnitb.TabIndex = 44;
             // 
             // dnilb
             // 
             this.dnilb.AutoSize = true;
-            this.dnilb.Location = new System.Drawing.Point(98, 65);
+            this.dnilb.Location = new System.Drawing.Point(65, 42);
+            this.dnilb.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dnilb.Name = "dnilb";
-            this.dnilb.Size = new System.Drawing.Size(41, 20);
+            this.dnilb.Size = new System.Drawing.Size(29, 13);
             this.dnilb.TabIndex = 43;
             this.dnilb.Text = "DNI:";
             // 
             // nombretb
             // 
-            this.nombretb.Location = new System.Drawing.Point(197, 113);
+            this.nombretb.Location = new System.Drawing.Point(131, 73);
+            this.nombretb.Margin = new System.Windows.Forms.Padding(2);
             this.nombretb.Name = "nombretb";
-            this.nombretb.Size = new System.Drawing.Size(125, 26);
+            this.nombretb.Size = new System.Drawing.Size(85, 20);
             this.nombretb.TabIndex = 46;
             // 
             // nombrelb
             // 
             this.nombrelb.AutoSize = true;
-            this.nombrelb.Location = new System.Drawing.Point(98, 116);
+            this.nombrelb.Location = new System.Drawing.Point(65, 75);
+            this.nombrelb.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nombrelb.Name = "nombrelb";
-            this.nombrelb.Size = new System.Drawing.Size(69, 20);
+            this.nombrelb.Size = new System.Drawing.Size(47, 13);
             this.nombrelb.TabIndex = 45;
             this.nombrelb.Text = "Nombre:";
             // 
             // telefonotb
             // 
-            this.telefonotb.Location = new System.Drawing.Point(197, 167);
+            this.telefonotb.Location = new System.Drawing.Point(131, 109);
+            this.telefonotb.Margin = new System.Windows.Forms.Padding(2);
             this.telefonotb.Name = "telefonotb";
-            this.telefonotb.Size = new System.Drawing.Size(125, 26);
+            this.telefonotb.Size = new System.Drawing.Size(85, 20);
             this.telefonotb.TabIndex = 48;
             // 
             // telefonolb
             // 
             this.telefonolb.AutoSize = true;
-            this.telefonolb.Location = new System.Drawing.Point(98, 170);
+            this.telefonolb.Location = new System.Drawing.Point(65, 110);
+            this.telefonolb.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.telefonolb.Name = "telefonolb";
-            this.telefonolb.Size = new System.Drawing.Size(75, 20);
+            this.telefonolb.Size = new System.Drawing.Size(52, 13);
             this.telefonolb.TabIndex = 47;
             this.telefonolb.Text = "Teléfono:";
             // 
             // botonB
             // 
             this.botonB.AutoSize = true;
-            this.botonB.Location = new System.Drawing.Point(197, 289);
+            this.botonB.Location = new System.Drawing.Point(131, 188);
+            this.botonB.Margin = new System.Windows.Forms.Padding(2);
             this.botonB.Name = "botonB";
-            this.botonB.Size = new System.Drawing.Size(45, 24);
+            this.botonB.Size = new System.Drawing.Size(32, 17);
             this.botonB.TabIndex = 52;
             this.botonB.TabStop = true;
             this.botonB.Text = "B";
@@ -105,9 +112,10 @@
             // botonA
             // 
             this.botonA.AutoSize = true;
-            this.botonA.Location = new System.Drawing.Point(90, 289);
+            this.botonA.Location = new System.Drawing.Point(60, 188);
+            this.botonA.Margin = new System.Windows.Forms.Padding(2);
             this.botonA.Name = "botonA";
-            this.botonA.Size = new System.Drawing.Size(45, 24);
+            this.botonA.Size = new System.Drawing.Size(32, 17);
             this.botonA.TabIndex = 49;
             this.botonA.TabStop = true;
             this.botonA.Text = "A";
@@ -116,25 +124,28 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(86, 241);
+            this.label6.Location = new System.Drawing.Point(57, 157);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(78, 20);
+            this.label6.Size = new System.Drawing.Size(54, 13);
             this.label6.TabIndex = 51;
             this.label6.Text = "Categoría";
             // 
             // flowLayoutPanel1
             // 
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(50, 255);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(33, 166);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(325, 100);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(217, 65);
             this.flowLayoutPanel1.TabIndex = 50;
             // 
             // botonC
             // 
             this.botonC.AutoSize = true;
-            this.botonC.Location = new System.Drawing.Point(290, 289);
+            this.botonC.Location = new System.Drawing.Point(193, 188);
+            this.botonC.Margin = new System.Windows.Forms.Padding(2);
             this.botonC.Name = "botonC";
-            this.botonC.Size = new System.Drawing.Size(45, 24);
+            this.botonC.Size = new System.Drawing.Size(32, 17);
             this.botonC.TabIndex = 53;
             this.botonC.TabStop = true;
             this.botonC.Text = "C";
@@ -142,27 +153,31 @@
             // 
             // botonCancelar
             // 
-            this.botonCancelar.Location = new System.Drawing.Point(249, 421);
+            this.botonCancelar.Location = new System.Drawing.Point(166, 274);
+            this.botonCancelar.Margin = new System.Windows.Forms.Padding(2);
             this.botonCancelar.Name = "botonCancelar";
-            this.botonCancelar.Size = new System.Drawing.Size(99, 37);
+            this.botonCancelar.Size = new System.Drawing.Size(66, 24);
             this.botonCancelar.TabIndex = 55;
             this.botonCancelar.Text = "Cancelar";
             this.botonCancelar.UseVisualStyleBackColor = true;
+            this.botonCancelar.Click += new System.EventHandler(this.botonCancelar_Click);
             // 
             // botonAceptar
             // 
-            this.botonAceptar.Location = new System.Drawing.Point(102, 421);
+            this.botonAceptar.Location = new System.Drawing.Point(68, 274);
+            this.botonAceptar.Margin = new System.Windows.Forms.Padding(2);
             this.botonAceptar.Name = "botonAceptar";
-            this.botonAceptar.Size = new System.Drawing.Size(99, 37);
+            this.botonAceptar.Size = new System.Drawing.Size(66, 24);
             this.botonAceptar.TabIndex = 54;
             this.botonAceptar.Text = "Aceptar";
             this.botonAceptar.UseVisualStyleBackColor = true;
+            this.botonAceptar.Click += new System.EventHandler(this.botonAceptar_Click);
             // 
-            // Form1
+            // AltaCliente
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 497);
+            this.ClientSize = new System.Drawing.Size(299, 323);
             this.Controls.Add(this.botonCancelar);
             this.Controls.Add(this.botonAceptar);
             this.Controls.Add(this.botonC);
@@ -176,7 +191,8 @@
             this.Controls.Add(this.nombrelb);
             this.Controls.Add(this.dnitb);
             this.Controls.Add(this.dnilb);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "AltaCliente";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
--- TOO/Presentación/AltaCliente.cs
+++ TOO/Presentación/AltaCliente.cs
@@ -16,5 +16,42 @@ namespace Presentación
         {
             InitializeComponent();
         }
+
+        private void botonAceptar_Click(object sender, EventArgs e)
+        {
+            if (!nombretb.Text.Equals("") || !telefonotb.Text.Equals("") || !dnitb.Text.Equals(""))
+            {
+
+            }
+            else {
+                if (nombretb.Text.Equals("")) MessageBox.Show("Completa el campo Nombre");
+                else if (telefonotb.Text.Equals("")) MessageBox.Show("Completa el campo Telefono");
+                else MessageBox.Show("Completa el campo DNI");
+            }
+        }
+
+        private void botonCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public bool compruebaLosTextBox() {
+            //Comprueba si los TextBox son vacíos o si tienen algun dato mal introducido
+
+            //Comprobamos que no sean vacíos, sino dev falso
+            if (!nombretb.Text.Equals("") || !telefonotb.Text.Equals("") || !dnitb.Text.Equals(""))
+            {
+                //Como los otros campos no pueden ser parseados por ser strings, comprobamos que el telf es in int
+                if (!long.TryParse(telefonotb.Text, out long telf))
+                {
+                    return false;
+                }
+                else { 
+                    return true;
+                }
+
+
+            }else return false;
+        }
     }
 }
